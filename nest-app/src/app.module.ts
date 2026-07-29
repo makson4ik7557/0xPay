79d8e021e0d0aed25d42f95adf17f892c55140bd
Maksym Martyn <maksroomm@gmail.com>
2026-07-29 23:42:49 +0300

    feat(auth): add register and login endpoints with JWT

--- nest-app/src/app.module.ts
+++ nest-app/src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {ConfigModule} from "@nestjs/config";
 import { PrismaModule } from './prisma/prisma.module';
+import {AuthModule} from './auth/auth.module';
 
 @Module({
   imports: [
-      ConfigModule.forRoot({isGlobal:true}),
-      PrismaModule
+    AuthModule,
+    ConfigModule.forRoot({isGlobal:true}),
+    PrismaModule
   ],
   controllers: [AppController],
   providers: [AppService],
